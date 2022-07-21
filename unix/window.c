e982ce14f8b04b50274ef24e214066efda19395b
mohad12211 <mohammedkaabi64@gmail.com>
2022-07-21 10:45:55 +0300

    make non-main windows modal

--- unix/window.c
+++ unix/window.c
@@ -1,6 +1,8 @@
 // 11 june 2015
 #include "uipriv_unix.h"
 
+static int created = 0;
+
 struct uiWindow {
 	uiUnixControl c;
 
@@ -299,6 +301,11 @@ uiWindow *uiNewWindow(const char *title, int width, int height, int hasMenubar)
 
 	gtk_window_set_title(w->window, title);
 	gtk_window_resize(w->window, width, height);
+	if (created == 0) {
+		created =  1;
+	} else {
+		gtk_window_set_modal(w->window, 1);
+	}
 
 	w->vboxWidget = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
 	w->vboxContainer = GTK_CONTAINER(w->vboxWidget);
